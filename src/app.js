2a0607675e47dac0ce1ab5bd43136c58aa7bbf0d
Zachary Zboncak <zachary.zboncak@gmail.com>
2020-01-14 12:56:53 -0600

    sucessfully fetching data from untappd

---
+++ src/app.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const request = require('request');
+
+const app = express();
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.get('/beerId', (req, res) => {
+
+  let options = {
+    'method': 'GET',
+    'url': `https://api.untappd.com/v4/beer/info/2789250?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&oauth_consumer_key=${process.env.CLIENT_ID}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1579027479&oauth_nonce=xmUwT3Q1lCd&oauth_version=1.0&oauth_signature=W9Bzzn4PmqrOP43XnWmbMFZ1WC0=`,
+    // 'headers': {
+    // }
+  };
+  request(options, function (error, response) { 
+    if (error) throw new Error(error);
+    console.log(response.body);
+    res.send(response.body);
+  });
+  
+})
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.log(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+})
+
+module.exports = app;
---
+++ store.js
@@ -0,0 +1,40 @@
+let STORE = [
+    {
+        id: "1",
+        beer_name: "Bourbon County Brand Stout (2018)",
+        quantity: 7,
+        rating: 4.47,
+        description: "A liquid as dark and dense as a black hole with thick foam the color of a bourbon barrel.",
+        brewery_id: 5494,
+        brewery_name: ""
+    },
+    {
+        id: "2",
+        beer_name: "Dark Lord",
+        quantity: 1,
+        rating: 4.33,
+        description: "A demonic Russian-Style Imperial Stout brewerd with coffee, Mexican vanilla, and Indian sugar.",
+        brewery_id: 5494,
+        brewery_name: ""
+    },
+    {
+        id: "3",
+        beer_name: "Proprietor's Bourbon County Brand Stout (2018)",
+        quantity: 1,
+        rating: 4.54,
+        description: "This year's Proprietor's explores the broad spectrum of chocolate. Using a combination of dark chocolate and two types of cocoa nibs, this rich and decadent beer highlights the whole range of chocolate from smooth and silky milk chocolate flavors to deep and earthy dark chocolate.",
+        brewery_id: 5494,
+        brewery_name: ""
+    },
+    {
+        id: "4",
+        beer_name: "Pliney the Elder",
+        quantity: 4,
+        rating: 4.52,
+        description: "A very good IPA.",
+        brewery_id: 5494,
+        brewery_name: ""
+    },
+];
+
+module.exports = STORE;
